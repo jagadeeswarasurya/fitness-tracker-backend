3d1d115c14655cb6e98bed9b898e72c8e41338ee
jagadeeswarasurya <jagadeeswarasuryaj@gmail.com>
2024-09-28 00:11:52 +0530

    db changed

--- config/db.js
+++ config/db.js
@@ -1,33 +1,25 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-// Load environment variables from .env file
 dotenv.config();
 
-// const dbUrl = "localhost:27017"; // Local MongoDB URL
-const dbName = process.env.DB_NAME || "fitness_tracker"; // Database name
-
-// Cloud DB credentials
+const dbName = process.env.DB_NAME || "fitness_tracker";
 const dbPassword = process.env.DB_PASSWORD || "";
 const dbUsr = process.env.DB_USERNAME || "";
 const dbCluster = process.env.DB_CLUSTER || "";
 
-// Local MongoDB connection string
-const localUrl = `mongodb://${dbUrl}/${dbName}`; 
-
-// Cloud MongoDB connection string (e.g., MongoDB Atlas)
+// Cloud MongoDB URL
 const cloudUrl = `mongodb+srv://${dbUsr}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Mongoose connection function
+// Connect to MongoDB
 const connectViaMongoose = async () => {
   try {
-    const url = dbUsr && dbPassword && dbCluster ? cloudUrl : localUrl; 
-    await mongoose.connect(url); 
-    console.log("Mongoose Connected Successfully to", url);
+    await mongoose.connect(cloudUrl);
+    console.log("Mongoose Connected Successfully to cloud MongoDB");
   } catch (e) {
-    console.error("Error connecting to database", e);
-    process.exit(1); // Exit on connection failure
+    console.log("Error connecting to database", e);
+    process.exit(1);
   }
 };
 
-module.exports = connectViaMongoose;
+export default connectViaMongoose;
